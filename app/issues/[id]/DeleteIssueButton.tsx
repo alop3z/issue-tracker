8dd3fe55a89c01bb1efe4b4ed96bb894e137a83e
Armando Lopez <mandokp2@gmail.com>
2023-10-24 22:22:49 -0400

    improve user experience

--- app/issues/[id]/DeleteIssueButton.tsx
+++ app/issues/[id]/DeleteIssueButton.tsx
@@ -1,20 +1,24 @@
 'use client'
-import { AlertDialog,Button, Flex } from '@radix-ui/themes'
+import { Spinner } from '@/app/components'
+import { AlertDialog, Button, Flex } from '@radix-ui/themes'
 import axios from 'axios'
-import {useRouter} from 'next/navigation'
-import { useState} from 'react'
+import { useRouter } from 'next/navigation'
+import { useState } from 'react'
 
 const DeleteIssueButton = ({issueId}: {issueId:number}) => {
     const router = useRouter()
     const [error, setError] =useState(false)
+    const [isDeleting, setDeleting] = useState(false)
 
     const deleteIssue = async () => { 
         try {
+            setDeleting(true)
             await axios.delete('/api/issues/' + issueId)
             router.push('/issues')
             router.refresh()
             
         } catch (error) {
+            setDeleting(false)
             setError(true)
         }
 
@@ -23,7 +27,10 @@ const DeleteIssueButton = ({issueId}: {issueId:number}) => {
     <>
     <AlertDialog.Root>
         <AlertDialog.Trigger>
-        <Button color='red'>Delete Issue</Button>
+        <Button color='red' disabled={isDeleting}>
+            Delete Issue
+            {isDeleting && <Spinner />}
+        </Button>
 
         </AlertDialog.Trigger>
         <AlertDialog.Content>
@@ -36,7 +43,9 @@ const DeleteIssueButton = ({issueId}: {issueId:number}) => {
                     <Button variant='soft' color='gray'>Cancel</Button>   
                 </AlertDialog.Cancel>
                 <AlertDialog.Action>
-                    <Button color='red' onClick={deleteIssue}>Delete Issue</Button>
+                    <Button color='red' onClick={deleteIssue} >
+                        Delete Issue
+                    </Button>
                 </AlertDialog.Action>
             </Flex>
         </AlertDialog.Content>
