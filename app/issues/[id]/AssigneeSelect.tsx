c18eecbbb5f74d351a32cbb3f8348b8bfecea813
Armando Lopez <mandokp2@gmail.com>
2024-02-08 21:47:27 -0500

    show toast notifications

--- app/issues/[id]/AssigneeSelect.tsx
+++ app/issues/[id]/AssigneeSelect.tsx
@@ -1,12 +1,13 @@
 'use client'
 import React, { useEffect, useState } from 'react'
 import { Select} from '@radix-ui/themes'
-import { User } from '@prisma/client'
+import { Issue, User } from '@prisma/client'
 import axios from 'axios'
 import { useQuery } from '@tanstack/react-query'
 import {Skeleton} from '@/app/components'
+import toast, {Toaster} from 'react-hot-toast'
 
-const AssigneeSelect = () => {
+const AssigneeSelect = ({issue}: {issue: Issue }) => {
     const {data: users, error, isLoading} = useQuery<User[]>({
         queryKey: ['users'],
         queryFn: () => axios.get('/api/users').then(res => res.data),
@@ -20,16 +21,23 @@ const AssigneeSelect = () => {
    
 
 return (
-    <Select.Root>
-        <Select.Trigger placeholder='Assign..'/>
-        <Select.Content>
-            <Select.Group>
-                <Select.Label>Suggestions</Select.Label>
-                {users?.map(user => <Select.Item key={user.id} value={user.id}> {user.name}</Select.Item>)}
-                
-            </Select.Group>
-        </Select.Content>
-    </Select.Root>
+    <>
+    
+        <Select.Root defaultValue={issue.assignedToUserId || ""} onValueChange={(userId) => {
+            axios.patch('/api/issues/' + issue.id, {assignedToUserId: userId || null }).catch(() => {toast.error('Changes could not be saved.')})
+        }}>
+            <Select.Trigger placeholder='Assign..'/>
+            <Select.Content>
+                <Select.Group>
+                    <Select.Label>Suggestions</Select.Label>
+                    <Select.Item value="">Unassigned</Select.Item>
+                    {users?.map(user => <Select.Item key={user.id} value={user.id}> {user.name}</Select.Item>)}
+                    
+                </Select.Group>
+            </Select.Content>
+        </Select.Root>
+        <Toaster></Toaster>
+    </>
   )
 }
 
